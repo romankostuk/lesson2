b345da70729558b0d87879fc45241c759be970ca
romankostuk <moryak.man@list.ru>
2024-06-03 19:40:12 +0300

    Add files via upload

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,5 @@
-name_1 = "vsego zadach:12,"
-name_2 = "zatracheno chasov: 1.5,"
-name_3 = "Kurs: Python,"
-name_4 = "srednee vremya vipolneniya 0.125 chasa."
-print(name_3, name_1, name_2, name_4)
+a = 12
+b = 1.5
+c = "Python"
+d = 0.125
+print('Курс:',c,',', 'всего задач:',a,',', 'затраченно часов:',b,',', 'среднее время выполнения:',d, 'часа.')
